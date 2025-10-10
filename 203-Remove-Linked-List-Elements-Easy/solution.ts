b518b33a163cbc6459e24c96fa4e33717d347d60
upnico <nicolas.leroux.1@proton.me>
2025-10-10 17:08:33 +0200

    203-Remove-Linked-List-Elements-Easy

---
+++ 203-Remove-Linked-List-Elements-Easy/solution.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+    const dummy = new ListNode();
+    dummy.next = head;
+
+    let current = dummy;
+
+    while (current.next) {
+        if (current.next.val === val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return dummy.next;
+
+};
